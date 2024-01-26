970944f9440225ba20a32bdc25b79470f7a2d984
lailien3 <laila.aleissa333@gmail.com>
2024-01-26 12:56:58 +0000

    Started implementing functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import './App.css';
-import mainBG from '../src/media/mainBG.mp4'
+import mainBG from '../src/media/mainBG.mp4';
+import React, { useState } from 'react';
+import { generateMagicResponse } from './QuestionAndAnswer';
 
 function App() {
+  const [userName, setUserName] = useState("Laila");
+  const [userQuestion, setUserQuestion] = useState("");
+  const [magicResponse, setMagicResponse] = useState("");
+
+  const handleQuestionSubmit = () => {
+    const response = generateMagicResponse(userName, userQuestion);
+    setMagicResponse(response);
+  };
+
   return(
 
     <div className='main-container'>
       <video className='main-video' src={mainBG} autoPlay loop muted/>
 
       <div className='welcome-container'>
-        Welcome user!
+        {userName ? `Welcome ${userName}!` : `Welcome!`}
+
       </div>
       <div className='question-container'>
-        What is your question?
+        <input
+          type = 'text'
+          placeholder = 'What is your question?'
+          value = {userQuestion}
+          onChange = {(e) => setUserQuestion(e.target.value)}
+        />
+        <button>onClick={handleQuestionSubmit} Ask </button>
       </div>
       <div className='response-container'>
-        Here is the answer.
+        {magicResponse && `Magic Eight ball says ... ${magicResponse}.`}
       </div>
 
     </div>
